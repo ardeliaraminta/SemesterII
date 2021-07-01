c93b363931c6c17288df4d63d5d6c2c8ddedc4f9
ardeliaraminta <ardelia.araminta@binus.ac.id>
2021-07-01 16:32:02 +0700

    book main

--- JAVA/Sir Jude's OOP/finalExam/Book.java
+++ JAVA/Sir Jude's OOP/finalExam/Book.java
@@ -52,11 +52,5 @@ public class Book {
                 ", $" + price +
                 ", year=" + year;
     }
-    public static void main(String []args) {
-        Book newbook = new Book("Raymond Bahana", "Superheroes are Real", 15.50, 1719);
-        Book newbook2 = new Book("Nunung Nurul", "Falling in love with Polymorphism", 12.80, 1902);
-        Book newbook3 = new Book("Jude Martinez", "Bleh Bleh Bleh Bleh", 9.50, 1996);
-
-    }
 
 }
---
+++ JAVA/Sir Jude's OOP/finalExam/MainBook.java
@@ -0,0 +1,10 @@
+package finalExam;
+
+public class MainBook {
+    public static void main(String []args) {
+        Book newbook = new Book("Raymond Bahana", "Superheroes are Real", 15.50, 1719);
+        Book newbook2 = new Book("Nunung Nurul", "Falling in love with Polymorphism", 12.80, 1902);
+        Book newbook3 = new Book("Jude Martinez", "Bleh Bleh Bleh Bleh", 9.50, 1996);
+    }
+
+}
